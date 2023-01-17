1c5b6d00ec53094f02a1e777b3e29b41d735fbab
Destroyer-9 <shiersh.official@outlook.com>
2023-01-17 23:09:41 +0530

    Added Java Folder

---
+++ java_DSA/Hello_World.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Hello_World{
+    public static void main(String[] args){
+        int n;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        System.out.println(n);
+    
+        System.out.print("Hello World!");
+    }
+}
